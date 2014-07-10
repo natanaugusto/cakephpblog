9af5a40c50739b0e93c95961d6bc5e23ec4a023b
Natan Augusto <natanaugusto@gmail.com>
2014-07-10 16:45:19 -0300

    Still studying

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitfd68496c18e53bf0f03c20a19c7fd533::getLoader();
+return ComposerAutoloaderInit8497a3de4ae39383997bca2aea5ecc7f::getLoader();
